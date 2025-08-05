798efec61ffd88bfee93082a6db2ae1087e7ceae
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-08-05 21:02:45 +0300

    Add project files.

---
+++ OOP Assignment03/Classes/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment03.Classes
+{
+
+    enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+
+    }
+
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public SecurityLevel Security { get; set; }
+
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Gender must be 'M' or 'F'");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Employee Info:\nID: {ID}\nName: {Name}\nGender: {(Gender == 'M' ? "Male" : "Female")}\nSecurity Level: {Security}\nSalary: {String.Format("{0:C}", Salary)}\nHire Date: {HireDate.ToShortDateString()}";
+        }
+
+
+
+    }
+}
---
+++ OOP Assignment03/Program.cs
@@ -0,0 +1,34 @@
+﻿using OOP_Assignment03.Classes;
+
+namespace OOP_Assignment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            try
+            {
+                Employee emp = new Employee
+                {
+                    ID = 1,
+                    Name = "Abdelrahman Nasr",
+                    Gender = 'M',
+                    Security = SecurityLevel.Developer,
+                    Salary = 7500.50m,
+                    HireDate = new DateTime(2022, 3, 15)
+                };
+
+                Console.WriteLine(emp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        #endregion
+
+    }
+    }
+
