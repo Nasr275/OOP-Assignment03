8697b059929c1c218d12afdc7b924869ec0f3f0a
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-08-05 22:37:35 +0300

    Q3

--- OOP Assignment03/Classes/Employee.cs
+++ OOP Assignment03/Classes/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace OOP_Assignment03.Classes
 {
@@ -12,21 +13,55 @@ namespace OOP_Assignment03.Classes
         Guest,
         Developer,
         Secretary,
-        DBA
+        DBA,
+        security_officer
 
     }
 
     internal class Employee
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
 
         public SecurityLevel Security { get; set; }
 
-        public decimal Salary { get; set; }
-        public DateTime HireDate { get; set; }
+        public decimal salary { get; set; }
+        public DateTime hireDate { get; set; }
 
         private char gender;
+
+
+        public int ID
+        {
+            get => id;
+            set => id = value >= 0 ? value : throw new ArgumentException("ID must be non-negative.");
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("Name cannot be empty.");
+        }
+
+        public decimal Salary
+        {
+            get => salary;
+            set => salary = value >= 0 ? value : throw new ArgumentException("Salary must be non-negative.");
+        }
+
+
+        public DateTime HireDate
+        {
+            get => hireDate;
+            set
+            {
+                if (value <= DateTime.Today)
+                    hireDate = value;
+                else
+                    throw new ArgumentException("Hire date cannot be in the future.");
+            }
+        }
+
         public char Gender
         {
             get { return gender; }
@@ -37,6 +72,21 @@ namespace OOP_Assignment03.Classes
                 else
                     throw new ArgumentException("Gender must be 'M' or 'F'");
             }
+        }
+
+
+
+        public Employee()
+        {      
+        }
+        public Employee(int id, string name, char gender, SecurityLevel security, decimal salary, DateTime hireDate)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Security = security;
+            Salary = salary;
+            HireDate = hireDate;
         }
 
         public override string ToString()
--- OOP Assignment03/Program.cs
+++ OOP Assignment03/Program.cs
@@ -31,18 +31,36 @@ namespace OOP_Assignment03
 
             #region Q2 Develop a Class to represent the Hiring Date Data
 
-            try
-            {
-                HiringDate hireDate = new HiringDate(01, 09, 2024);
-                Console.WriteLine("Hiring Date: " + hireDate);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
+            //try
+            //{
+            //    HiringDate hireDate = new HiringDate(01, 09, 2024);
+            //    Console.WriteLine("Hiring Date: " + hireDate);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error: " + ex.Message);
+            //}
 
             #endregion
 
+            #region Q3
+
+            Employee[] EmpArr = new Employee[3];
+
+            EmpArr[0] = new Employee(1, "Ahmed Gamal", 'M', SecurityLevel.DBA, 15000.50m, new DateTime(2021, 6, 1));
+
+            EmpArr[1] = new Employee(2, "Salma Fathy", 'F', SecurityLevel.Guest, 5000.00m, new DateTime(2023, 2, 12));
+
+            EmpArr[2] = new Employee(3, "Khaled Tarek", 'M', SecurityLevel.security_officer, 18000.00m, new DateTime(2020, 11, 5));
+
+
+            foreach (var emp in EmpArr)
+            {
+                Console.WriteLine(emp);
+                Console.WriteLine("---------------------------------------------------------------");
+            }
+
+            #endregion
         }
     }
 }
