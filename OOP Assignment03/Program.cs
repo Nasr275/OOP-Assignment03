fe6c2b05dcd149476b084ddbd2b16c494ae9df16
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-08-05 21:52:26 +0300

    Q2

--- OOP Assignment03/Program.cs
+++ OOP Assignment03/Program.cs
@@ -7,28 +7,43 @@ namespace OOP_Assignment03
         static void Main(string[] args)
         {
 
-            #region
+            #region Q1
+            //    try
+            //    {
+            //        Employee emp = new Employee
+            //        {
+            //            ID = 1,
+            //            Name = "Abdelrahman Nasr",
+            //            Gender = 'M',
+            //            Security = SecurityLevel.Developer,
+            //            Salary = 7500.50m,
+            //            HireDate = new DateTime(2022, 3, 15)
+            //        };
+
+            //        Console.WriteLine(emp);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("Error: " + ex.Message);
+            //    }
+            //}
+            #endregion
+
+            #region Q2 Develop a Class to represent the Hiring Date Data
+
             try
             {
-                Employee emp = new Employee
-                {
-                    ID = 1,
-                    Name = "Abdelrahman Nasr",
-                    Gender = 'M',
-                    Security = SecurityLevel.Developer,
-                    Salary = 7500.50m,
-                    HireDate = new DateTime(2022, 3, 15)
-                };
-
-                Console.WriteLine(emp);
+                HiringDate hireDate = new HiringDate(01, 09, 2024);
+                Console.WriteLine("Hiring Date: " + hireDate);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
-        }
-        #endregion
 
+            #endregion
+
+        }
     }
-    }
+}
 
